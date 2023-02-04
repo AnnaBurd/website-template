b4f6afb9338f6fa7aeb1f5b731773485304ed504
Anna Burdanova <burdanovaas@gmail.com>
2023-02-04 15:06:07 +0700

    animated line in main hero section

---
+++ src/js/text-line-animation.js
@@ -0,0 +1,58 @@
+/*
+  Script to create animation of typewriting
+*/
+
+const textLines = ["Hanh phuc la cai gi nua.", "Hanh phuc la cai gi nua - 12.", "Lorem ipsum :)"];
+let textLineNumber = 0;
+let animatedTextElement = document.getElementById("hero-text");
+let oldTextLine = animatedTextElement.innerHTML;
+
+let pause = 500; // ms
+let pauseMiddle = 1000;
+let speed = 50;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function typeWriter(newTextLine){
+
+  pause = (newTextLine.length + oldTextLine.length) * speed + pauseMiddle * 2; // Adjust timeput pause before next call
+
+  // Remove old text
+  // console.log("Remove old text");
+  for(let i = oldTextLine.length; i >= 0; i--){
+    oldTextLine = oldTextLine.slice(0,i);
+    animatedTextElement.innerHTML = oldTextLine + '︳';
+    await sleep(speed / 2);
+  };
+
+  console.log("Wait a little bit");
+  await sleep(pauseMiddle); // Wait a little bit
+
+  // Print the new text
+  console.log("Print new text");
+  for (let j = 0; j <= newTextLine.length; j++){
+    animatedTextElement.innerHTML = newTextLine.slice(0,j)+ '︳';
+    await sleep(speed);
+  }
+
+  oldTextLine = newTextLine; // Prepare for next loop
+
+  setTimeout(function() {
+
+    typeWriter(textLines[textLineNumber]);
+    textLineNumber++;
+    if (textLineNumber >= textLines.length){
+      textLineNumber = 0; // Reset String Rotation
+    }
+  
+  }, pause);
+}
+
+setTimeout(function() {
+
+  typeWriter(textLines[textLineNumber], speed);
+  textLineNumber++;
+
+}, pause);
